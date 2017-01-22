2f9dd2a14c70d5ad9b1a2b190e777e56dc9dbb27
Louis Tur <louis.tur@gmail.com>
2017-01-22 18:35:18 -0500

    base proj commit

---
+++ AC3.2-AnimationBasics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AC3.2-AnimationBasics
+//
+//  Created by Louis Tur on 1/22/17.
+//  Copyright © 2017 Access Code. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
