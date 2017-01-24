82130336adb7de239d3f0b81dfaa341fd6a5dc8a
Louis Tur <louis.tur@gmail.com>
2017-01-24 10:33:39 -0500

    lesson-ready

--- AC3.2-AnimationBasics/Demos/TouchAnimatorViewController.swift
+++ AC3.2-AnimationBasics/Demos/TouchAnimatorViewController.swift
@@ -13,7 +13,6 @@ class TouchAnimatorViewController: UIViewController {
   
   var animator: UIViewPropertyAnimator? = nil
   let squareSize = CGSize(width: 100.0, height: 100.0)
-  var viewIsCurrentlyHeld: Bool = false
   
   // MARK: - View LifeCycle
   override func viewDidLoad() {
@@ -42,54 +41,11 @@ class TouchAnimatorViewController: UIViewController {
   
   // MARK: - Movement
   internal func move(view: UIView, to point: CGPoint) {
-    if animator!.isRunning {
-      animator?.addAnimations {
-        self.view.layoutIfNeeded()
-      }
-    }
-    
-    view.snp.remakeConstraints { (view) in
-      view.center.equalTo(point)
-      view.size.equalTo(squareSize)
-    }
 
-  }
-  
-  internal func pickUp(view: UIView) {
-    animator = UIViewPropertyAnimator(duration: 0.2, curve: .easeOut, animations: { 
-      view.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
-    })
-
-    animator?.startAnimation()
-  }
-  
-  internal func putDown(view: UIView) {
-    animator = UIViewPropertyAnimator(duration: 0.15, curve: .easeIn, animations: {
-      view.transform = CGAffineTransform.identity
-    })
-    
-    animator?.startAnimation()
   }
   
   
   // MARK: - Tracking Touches
-  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    guard let touch = touches.first else { return }
-    let touchWasInsideOfDarkBlueView = darkBlueView.frame.contains(touch.location(in: view))
-    
-    if touchWasInsideOfDarkBlueView {
-      pickUp(view: darkBlueView)
-    }
-  }
-  
-  override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-    putDown(view: darkBlueView)
-  }
-  
-  override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-    guard let touch = touches.first else { return }
-    move(view: darkBlueView, to: touch.location(in: view))
-  }
   
   
   // MARK: - Views
--- AC3.2-AnimationBasics/ViewController.swift
+++ AC3.2-AnimationBasics/ViewController.swift
@@ -10,13 +10,8 @@ import UIKit
 import SnapKit
 
 class ViewController: UIViewController {
-  static let animationDuration: TimeInterval = 1.0
-  
-  let darkBlueAnimator = UIViewPropertyAnimator(duration: animationDuration, curve: .linear, animations: nil)
-  let tealAnimator = UIViewPropertyAnimator(duration: animationDuration, curve: .easeInOut, animations: nil)
-  let yellowAnimator = UIViewPropertyAnimator(duration: animationDuration, curve: .easeIn, animations: nil)
-  let orangeAnimator = UIViewPropertyAnimator(duration: animationDuration, curve: .easeOut, animations: nil)
-  
+  static let animationDuration: TimeInterval = 2.0
+  static let squareSize = CGSize(width: 100.0, height: 100.0)
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -28,10 +23,6 @@ class ViewController: UIViewController {
   }
   
   private func configureConstraints() {
-    darkBlueView.snp.removeConstraints()
-    tealView.snp.removeConstraints()
-    yellowView.snp.removeConstraints()
-    orangeView.snp.removeConstraints()
     
     // blue
     darkBlueView.snp.makeConstraints { (view) in
@@ -40,28 +31,6 @@ class ViewController: UIViewController {
       view.size.equalTo(CGSize(width: 100.0, height: 100.0))
     }
     
-    // teal
-    tealView.snp.makeConstraints { (view) in
-      view.leading.equalToSuperview().offset(20.0)
-      view.top.equalTo(darkBlueView.snp.bottom).offset(20.0)
-      view.size.equalTo(darkBlueView.snp.size)
-    }
-    
-    // yellow
-    yellowView.snp.makeConstraints { (view) in
-      view.leading.equalToSuperview().offset(20.0)
-      view.top.equalTo(tealView.snp.bottom).offset(20.0)
-      view.size.equalTo(darkBlueView.snp.size)
-    }
-    
-    // orange
-    orangeView.snp.makeConstraints { (view) in
-      view.leading.equalToSuperview().offset(20.0)
-      view.top.equalTo(yellowView.snp.bottom).offset(20.0)
-      view.size.equalTo(darkBlueView.snp.size)
-    }
-    
-    
     // button
     animateButton.snp.makeConstraints { (view) in
       view.centerX.equalToSuperview()
@@ -69,184 +38,41 @@ class ViewController: UIViewController {
       view.width.greaterThanOrEqualTo(100.0)
     }
     
-    resetAnimationsButton.snp.makeConstraints { (view) in
-      view.centerX.equalToSuperview()
-      view.top.equalTo(animateButton.snp.bottom).offset(8.0)
-    }
-    
-    // switch
-    reverseAnimationsSwitch.snp.makeConstraints { (view) in
-      view.centerY.equalTo(animateButton.snp.centerY)
-      view.leading.equalToSuperview().offset(20.0)
-    }
-    
-    // slider
-    scrubbingSlider.snp.makeConstraints { (view) in
-      view.bottom.equalTo(animateButton.snp.top)
-      view.leading.equalToSuperview().offset(8.0)
-      view.trailing.equalToSuperview().inset(8.0)
-    }
   }
   
   private func setupViewHierarchy() {
     self.view.backgroundColor = .white
     
     self.view.addSubview(darkBlueView)
-    self.view.addSubview(tealView)
-    self.view.addSubview(yellowView)
-    self.view.addSubview(orangeView)
-    
-    self.view.addSubview(reverseAnimationsSwitch)
     self.view.addSubview(animateButton)
-    self.view.addSubview(resetAnimationsButton)
-    self.view.addSubview(scrubbingSlider)
   }
   
   private func addGesturesAndActions() {
     self.animateButton.addTarget(self, action: #selector(animateViews), for: .touchUpInside)
-    self.resetAnimationsButton.addTarget(self, action: #selector(reset), for: .touchUpInside)
-    self.reverseAnimationsSwitch.addTarget(self, action: #selector(reverse(sender:)), for: .valueChanged)
-    self.scrubbingSlider.addTarget(self, action: #selector(updateAnimationProgress(sender:)), for: .valueChanged)
   }
   
-  internal func reset() {
-    darkBlueAnimator.stopAnimation(true)
-    
-    configureConstraints()
-    self.view.layoutIfNeeded()
-  }
-  
-  internal func reverse(sender: UISwitch) {
-    darkBlueAnimator.isReversed = sender.isOn
-    tealAnimator.isReversed = sender.isOn
-    orangeAnimator.isReversed = sender.isOn
-    yellowAnimator.isReversed = sender.isOn
-  }
-  
-  internal func updateAnimationProgress(sender: UISlider) {
-    
-    if darkBlueAnimator.isRunning {
-      darkBlueAnimator.pauseAnimation()
-    } else {
-      animateDarkBlueViewWithSnapkit()
-      darkBlueAnimator.pauseAnimation()
-    }
-    
-    darkBlueAnimator.fractionComplete = CGFloat(sender.value)
-  }
   
   // MARK: - Animations
   
   // MARK: Property Animator
   internal func animateViews() {
-    animateDarkBlueViewWithSnapkit()
-    animateTealViewWithSnapkit()
-    animateYellowViewWithSnapkit()
-    animateOrangeViewWithSnapkit()
+    
   }
   
   internal func animateDarkBlueViewWithSnapkit() {
-    
-    self.darkBlueView.snp.remakeConstraints { (view) in
-      view.trailing.equalToSuperview().inset(20.0)
-      view.top.equalToSuperview().offset(20.0)
-      view.size.equalTo(CGSize(width: 100.0, height: 100.0))
-    }
-    
-    darkBlueAnimator.addAnimations { 
-      self.view.layoutIfNeeded()
-    }
-    
-    darkBlueAnimator.addAnimations({ 
-      self.darkBlueView.backgroundColor = Colors.red
-    }, delayFactor: 0.5)
-    
-    darkBlueAnimator.addCompletion { (position: UIViewAnimatingPosition) in
-      switch position {
-      case .start: print("At the start of the animation")
-      case .end: print("At the end of the animation")
-      case .current: print("Somewhere in the middle")
-      }
-    }
-    
-    darkBlueAnimator.startAnimation()
-  }
-  
-  internal func animateTealViewWithSnapkit() {
-    self.tealView.snp.remakeConstraints { (view) in
-      view.trailing.equalToSuperview().inset(20.0)
-      view.top.equalTo(self.darkBlueView.snp.bottom).offset(20.0)
-      view.size.equalTo(CGSize(width: 100.0, height: 100.0))
-    }
-    
-    tealAnimator.addAnimations { 
-      self.view.layoutIfNeeded()
-    }
-    
-    tealAnimator.startAnimation()
-  }
-  
-  internal func animateYellowViewWithSnapkit() {
-    self.yellowView.snp.remakeConstraints { (view) in
-      view.trailing.equalToSuperview().inset(20.0)
-      view.top.equalTo(self.tealView.snp.bottom).offset(20.0)
-      view.size.equalTo(CGSize(width: 100.0, height: 100.0))
-    }
-    
-    yellowAnimator.addAnimations {
-      self.view.layoutIfNeeded()
-    }
-    
-    yellowAnimator.startAnimation()
-  }
 
-  internal func animateOrangeViewWithSnapkit() {
-    self.orangeView.snp.remakeConstraints { (view) in
-      view.trailing.equalToSuperview().inset(20.0)
-      view.top.equalTo(self.yellowView.snp.bottom).offset(20.0)
-      view.size.equalTo(CGSize(width: 100.0, height: 100.0))
-    }
-    
-    orangeAnimator.addAnimations {
-      self.view.layoutIfNeeded()
-    }
-    
-    orangeAnimator.startAnimation()
   }
-
   
   // MARK: Frames
   internal func animateDarkBlueViewWithFrames() {
-    let newFrame = self.darkBlueView.frame.offsetBy(dx: 300.0, dy: 0.0)
-    
-    UIView.animate(withDuration: 1.0) {
-      self.darkBlueView.frame = newFrame
-    }
+
   }
   
   
-  // MARK: - Views
+  // MARK: - Lazy Inits
   internal lazy var darkBlueView: UIView = {
     let view: UIView = UIView()
     view.backgroundColor = Colors.darkBlue
-    return view
-  }()
-  
-  internal lazy var tealView: UIView = {
-    let view: UIView = UIView()
-    view.backgroundColor = Colors.teal
-    return view
-  }()
-  
-  internal lazy var yellowView: UIView = {
-    let view: UIView = UIView()
-    view.backgroundColor = Colors.yellow
-    return view
-  }()
-  
-  internal lazy var orangeView: UIView = {
-    let view: UIView = UIView()
-    view.backgroundColor = Colors.orange
     return view
   }()
   
@@ -256,23 +82,5 @@ class ViewController: UIViewController {
     return button
   }()
   
-  internal lazy var resetAnimationsButton: UIButton = {
-    let button = UIButton(type: .roundedRect)
-    button.setTitle("Reset", for: .normal)
-    return button
-  }()
-  
-  internal lazy var reverseAnimationsSwitch: UISwitch = {
-    let reverseSwitch = UISwitch()
-    reverseSwitch.isOn = false
-    return reverseSwitch
-  }()
-  
-  internal lazy var scrubbingSlider: UISlider = {
-    let slider = UISlider()
-    slider.minimumTrackTintColor = Colors.red
-    slider.maximumTrackTintColor = Colors.yellow
-    return slider
-  }()
 }
 
